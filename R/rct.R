5010a26770c0f1c8027ae8f6393152998528c0a7
ekstroem <github@ekstroem.com>
2017-02-04 17:20:16 +0100

    Pretest-posttest fix 2

--- R/rct.R
+++ R/rct.R
@@ -22,7 +22,6 @@ prepost.test <- function(baseline, post, treatment) {
 
     DF <- data.frame(baseline, post, treatment, treat)
 
-    print(DF)
     ## Complete case analysis
     ccanalysis <- lm(post ~ treatment + baseline, data=DF)
 
@@ -35,76 +34,54 @@ prepost.test <- function(baseline, post, treatment) {
     N1 <- sum(treatment)
     N0 <- N - N1
 
-    m1 <- lm(post ~ baseline, subset=(treatment==1))
-    m0 <- lm(post ~ baseline, subset=(treatment==0))
+    m1 <- lm(post ~ baseline, subset=(treat), data=DF)
+    m0 <- lm(post ~ baseline, subset=(!treat), data=DF)
 
 #    m1 <- lm(post ~ treatment*baseline)
 
-    eq1 <- predict(m1)
-    eq0 <- predict(m0)
+#    eq1 <- predict(m1)
+#    eq0 <- predict(m0)
 
-    eq1 <- predict(m1, newdata=data.frame(baseline, treatment=rep(1,N)))
-    eq0 <- predict(m1, newdata=data.frame(baseline, treatment=rep(0,N)))
+    eq1 <- predict(m1, newdata=data.frame(baseline, treat=rep(TRUE, N)))
+    eq0 <- predict(m1, newdata=data.frame(baseline, treat=rep(FALSE, N)))
+#    eq1 <- 1
+#    eq0 <- 1
 
     delta <- N1 / N
 
 
     pi1 <- 1
     pi0 <- 1
-    print(sum(DF$miss[DF$treat]))
-    if ( sum(DF$miss[DF$treat])>0 ) {
-        print(DF$R)
-        missmodel <- glm( R  ~ baseline*treat, family="binomial", subset=(treat==TRUE), data=DF)
-        pi1 <- predict(missmodel, newdata=data.frame(x, treat=rep(1, 100)), type="response")
+    if ( sum(miss[DF$treat])>0 ) {
+        missmodel <- glm( R  ~ baseline, family="binomial", subset=(treat==TRUE), data=DF)
+        pi1 <- predict(missmodel, newdata=data.frame(DF$baseline, treat=rep(TRUE, nrow(DF))), type="response")
     }
-    if ( sum(DF$miss[!DF$treat])>0 ) {
-        missmodel <- glm( R  ~ baseline*treat, family="binomial", subset=(treat==FALSE), data=DF)
-        pi0 <- predict(missmodel, newdata=data.frame(x, treat=rep(1, 100)), type="response")
+    if ( sum(miss[!DF$treat])>0 ) {
+        missmodel <- glm( R  ~ baseline, family="binomial", subset=(treat==FALSE), data=DF)
+        pi0 <- predict(missmodel, newdata=data.frame(DF$baseline, treat=rep(FALSE, nrow(DF))), type="response")
     }
-
-    print(pi0)
-    print(pi1)
-
-    print("asdasd")
-
-#    pi0 <- predict(missmodel, newdata=data.frame(x, treat=rep(0, 100)), type="response")
-#    pi0
-#    pi1 <- predict(missmodel, newdata=data.frame(x, treat=rep(1, 100)), type="response")
-#    pi1
-#    } else if sum(Z) {
-
-#    }
-
-
-#    cat("kjhkjh")
-#    print(pi1)
-
 
     post[is.na(post)] <- 0
 
-    mu1 <- 1/N1*( sum((R * treatment * post ) / pi1) -
-                  sum((treatment - delta)*eq1) -
-                  sum((R - pi1)*treatment*eq1/pi1)
+    mu1 <- 1/N1*( sum((R * treat * post ) / pi1) -
+                  sum((treat - delta)*eq1) -
+                  sum((R - pi1)*treat*eq1/pi1)
                  )
 
-#    mu1 <- 1/N1*( sum((treatment[R] * post[R] ) / pi1[R]) -
-#                  sum((treatment - delta)*eq1) -
-#                  sum((R - pi1)*treatment*eq1/pi1)
-#                 )
 
-#    print(length(eq1))
-#    print(eq1)
-#    print(length(treatment-delta))
-#    print((treatment-delta)*eq1)
-
-    mu2 <- 1/N0*(sum(R*(1-treatment)*post/pi0) +
-                     sum((treatment - delta)*eq0) -
-                     sum((R-pi0)*(1-treatment)*eq0/pi0)
+    mu2 <- 1/N0*(sum(R*(1-treat)*post/pi0) +
+                     sum((treat - delta)*eq0) -
+                     sum((R-pi0)*(1-treat)*eq0/pi0)
                  )
     betahat <- mu1 - mu2
 
 #    betahat <- coef(m1)[2] - coef(m2)[2]
 
+#    rval <- list(statistic = tstat, parameter = df, p.value = pval,
+#                 conf.int = cint, estimate = betahat, null.value = mu,
+#                 alternative = alternative, method = method, data.name = dname)
+#    class(rval) <- "htest"
+#    return(rval)
     betahat
 }
 
