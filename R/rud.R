bfaf3b5bd05a57487a8df35e04f7032aa2d54e3d
ekstroem <github@ekstroem.com>
2017-04-27 21:50:05 +0200

    rud updated

--- R/rud.R
+++ R/rud.R
@@ -1,21 +1,24 @@
 #' Simulate randomized urn design
 #'
-#' Creates a plot of the crayon colors in \code{\link{brocolors}}
+#' Simulates a randomized treatment based on an urn model.
+#'
+#' The urn model can be described as follows: For k different treatments, the urn design is initiated with a number of balls in an urn corresponding to the start weight (the alpha argument), where each treatment has a specific colour. Whenever a patient arrives, a random ball is drawn from the urn and the colour decides the treatment for the patient. For each of the treatments that weren't chosen we add beta balls of the corresponding colour(s) to the urn to update the probabilities for the next patient.
 #'
 #' @param n the number of individuals to randomize
 #' @param alpha a non-negative integer vector of weights for each treatment group. The length of the vector corresponds to the number of treatment groups.
-#' @param beta a non-negative integer    margin paramaters; vector of length 4 (see \code{\link[graphics]{par}})
-#' @param labels margin paramaters; vector of length 4 (see \code{\link[graphics]{par}})
-#' @param dataframe margin paramaters; vector of length 4 (see \code{\link[graphics]{par}})
+#' @param beta a non-negative integer of weights added to the groups that were not given treatment
+#' @param labels a vector of treatment labels. Must be the same length as the length of alpha.
+#' @param data.frame A logical that determines if the function should return a vector of group indices (the default, if FALSE) or a data frame (if TRUE).
 #' @param startid margin paramaters; vector of length 4 (see \code{\link[graphics]{par}})
 #'
-#' @return None
+#' @return A vector with group indices. If the argument \code{data.frame=TRUE} is used then a data frame with three variables is returned: id, group, and treatment (the group label). 
 #'
 #' @examples
-#' plot_crayons()
+#' rud(5)
+#' rud(5, alpha=c(1,1,10), beta=5)
 #'
 #' @export
-rud <- function(n, alpha=c(1, 1), beta=1, labels=seq(1, length(alpha)), dataframe=FALSE, startid=1) {
+rud <- function(n, alpha=c(1, 1), beta=1, labels=seq(1, length(alpha)), data.frame=FALSE, startid=1) {
 
     ncat <- length(alpha)  # Number of categories
     
@@ -34,7 +37,7 @@ rud <- function(n, alpha=c(1, 1), beta=1, labels=seq(1, length(alpha)), datafram
         urn[-res[i]] <- urn[-res[i]] + beta
     }
     
-    if (dataframe)
+    if (data.frame)
         data.frame(id=(1:n)+(startid-1), group=res, treatment=labels[res])
     else
         res
